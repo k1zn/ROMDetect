9382bead8865d99aa7ea24a18e035305db00da7d
k1zn <46452865+k1zn@users.noreply.github.com>
2022-06-24 23:14:45 +0500

    Fix bug

--- ROMDetect.py
+++ ROMDetect.py
@@ -3,15 +3,18 @@
 import sys, zipfile 
 
 check_files = ['META-INF/com/google/android/update-binary', 'META-INF/com/google/android/updater-script']
+is_brick = False
 
 if len(sys.argv) > 1:
   with zipfile.ZipFile(sys.argv[1:][0]) as zf:
       for file in zf.infolist():
         if file.filename in check_files:
           byte_text = zf.read(file)
-          if 'blockdev' in byte_text.decode('utf-8'):
-            print("[BRICK] DON'T INSTALL THIS! This installer contains \"blockdev\" function, which brick the phone.")
-          else:
-            print("[CLEAN] ROMDetect.py didn't found \"blockdev\" function in update scripts.")
+          if 'blockdev' in byte_text.decode('utf-8') and not is_brick:
+            is_brick = True
+  if is_brick:
+    print("[BRICK] DON'T INSTALL THIS! This installer contains \"blockdev\" function, which brick the phone.")
+  else:
+    print("[CLEAN] ROMDetect.py didn't found \"blockdev\" function in update scripts.")
 else:
   print('Usage: ROMDetect.py <filename>')
