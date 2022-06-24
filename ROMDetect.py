25bd0b41b52a98514994e9de40d5202702eb5dde
k1zn <46452865+k1zn@users.noreply.github.com>
2022-06-24 23:12:09 +0500

    Add "updater-script"

--- ROMDetect.py
+++ ROMDetect.py
@@ -2,14 +2,16 @@
 
 import sys, zipfile 
 
+check_files = ['META-INF/com/google/android/update-binary', 'META-INF/com/google/android/updater-script']
+
 if len(sys.argv) > 1:
   with zipfile.ZipFile(sys.argv[1:][0]) as zf:
       for file in zf.infolist():
-        if file.filename == 'META-INF/com/google/android/update-binary':
+        if file.filename in check_files:
           byte_text = zf.read(file)
           if 'blockdev' in byte_text.decode('utf-8'):
             print("[BRICK] DON'T INSTALL THIS! This installer contains \"blockdev\" function, which brick the phone.")
           else:
-            print("[CLEAN] ROMDetect.py didn't found \"blockdev\" function in update-binary.")
+            print("[CLEAN] ROMDetect.py didn't found \"blockdev\" function in update scripts.")
 else:
   print('Usage: ROMDetect.py <filename>')
